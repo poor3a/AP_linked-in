eeabdd616f766668ffe65899ca9d3d22f8db61a9
unknown <parsaasa84@gmail.com>
2024-06-04 21:17:47 +0330

    utils commit

---
+++ src/main/java/utils/JWTController.java
@@ -0,0 +1,39 @@
+package utils;
+
+import com.sun.net.httpserver.HttpExchange;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+
+public class JWTController {
+    private static final Key key = Keys.hmacShaKeyFor("ParsaExir@13842005-Am13841384-LINKED-IN".getBytes(StandardCharsets.UTF_8));
+    private static final long EXPIRATION_TIME = 1_000 * 60 * 60  * 3; // 3 hour
+    private static final SignatureAlgorithm algorithm = SignatureAlgorithm.HS256;
+
+    public static String createToken(String email) {
+        String token = Jwts.builder()
+                .setSubject(email)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key, algorithm)
+                .compact();
+        return token;
+    }
+
+    public static String verifyToken(HttpExchange exchange) {
+        try {
+            String token = exchange.getRequestHeaders().get("JWT").get(0);
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+        } catch (JwtException e) {
+            return null;
+        }
+    }
+
+}
